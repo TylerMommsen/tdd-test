c1f51b192e5b9b4c7c250035f06b5e64affbd4c0
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-28 21:40:24 +0300

    Added initial functions into index.js to be tested

---
+++ index.js
@@ -0,0 +1,58 @@
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+    divide(a, b) {
+        return a / b;
+    },
+    multiply(a, b) {
+        return a * b;
+    }
+}
+
+function caesarCipher(str, shiftFactor) {
+    let cipherText = '';
+    for (let i = 0; i < str.length; i++) {
+        cipherText + String.fromCharCode(charCodeAt[i] + 3);
+    }
+    return cipherText;
+}
+
+function analyzeArray(arr) {
+    function calculateAverage(array) {
+        if (numbers.length === 0) return 0;
+
+        let sum = 0;
+        array.forEach(num => {
+            sum += num;
+        });
+
+        return sum / array.length;
+    }
+
+    const object = {
+        average: calculateAverage(arr),
+        min: min(...arr),
+        max: max(...arr),
+        length: arr.length
+    }
+}
+
+export default {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray
+};
